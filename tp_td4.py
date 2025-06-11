0876bbabe20eff0bd4debad5c3a135885ce635cc
BenjaminChejtman <166836220+BenjaminChejtman@users.noreply.github.com>
2025-06-11 03:05:49 +0100

    Update tp_td4.py

--- tp_td4.py
+++ tp_td4.py
@@ -2,8 +2,13 @@ from scapy.all import *
 import statistics
 import time
 
-def ping(host):
-    print(f"Ping {host}")
+def snd_pkt(host, pkt_ttl):
+    pkt = IP(dst=host, ttl=pkt_ttl)/ICMP()
+    reply = sr1(pkt, timeout=2, verbose=0)
+    return reply
+
+def ping(host, rango, pkt_ttl):    
+    mensaje = ''
     cantSent:int = 0
     cantRcv:int = 0
     lost:int = 0
@@ -11,47 +16,38 @@ def ping(host):
     maximo:int = 0
     rtts:list[int] = []    
 
-    for i in range(0,10):   # Cosnulta: Cuantos paquetes tenemos que enviar?
-        pkt = IP(dst="8.8.8.8", ttl=128)/ICMP()
+    for i in range(0, rango):   # Cosnulta: Cuantos paquetes tenemos que enviar?
         cantSent+=1
         start = time.time()
-        reply = sr1(pkt, timeout=1, verbose=0)
+        reply = snd_pkt(host, pkt_ttl)
         end = time.time()
 
         if reply:
             cantRcv+=1
-
+            lng = len(reply)
             rtt = (end - start) * 1000
+            ttl = reply.ttl
+            
             rtts.append(rtt)
-
+            
             if rtt > maximo:
                 maximo = rtt
-
+            
             if rtt < minimo:
                 minimo = rtt
-            
+        
         else:
             lost+=1
-    
+            
     if rtts:
         promedio = statistics.mean(rtts)
         st = statistics.stdev(rtts) if len(rtts) > 1 else 0.0
+        
     else:
         promedio = st = 0.0
 
+    mensaje = ('Paquetes enviados = ' + str(cantSent) + '\nPaquetes recibidos = ' + str(cantRcv) + '\nPaquetes perdidos = ' + str(lost) + '\nPorcentaje perdidos = ' + str((lost/cantSent)*100) + '%' + '\nRTT promedio = ' + str(promedio) + 'ms' + '\nRTT maximo = ' + str(maximo) + 'ms' + '\nRTT minimo = ' + str(minimo) + 'ms' + '\nDesvio Standard = ' + str(st))
 
-    print('Paquetes enviados = ' + str(cantSent))
-    print('Paquetes recibidos = ' + str(cantRcv))
-    print('Paquetes perdidos = ' + str(lost))
-    print('Porcentaje perdidos = ' + str((lost/cantSent)*100) + '%')
-    print('RTT promedio = ' + str(promedio) + 'ms')
-    print('RTT maximo = ' + str(maximo) + 'ms')
-    print('RTT minimo = ' + str(minimo) + 'ms')
-    print('Desvio Standard = ' + str(st))
+    return mensaje
 
-    return
-
-#ping("google.com")
-
-# No hice una funcion auxiliar como sugiere en el punto (c)
-# Falta cumplir lo que dice en el punto (a)
+print(ping('8.8.8.8', 2, 5))
